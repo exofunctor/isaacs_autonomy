292cdafc1cbbd5e4fd2a731d901bc228e4936831
Apollo <apollo@apollo.vision>
2020-12-07 11:39:39 -0800

    fixed attitude stream

--- attitude_stream.py
+++ attitude_stream.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
+from __future__ import print_function
+import roslib
+roslib.load_manifest('isaacs_autonomy')
 import rospy
-from geometry_msgs.msg import Quaternion
-from numpy import array, arctan2, arcsin
+from geometry_msgs.msg import QuaternionStamped
+from numpy import array, arctan2, arcsin, pi
 
 
 # TODO: Description
+# 100hz
 class AttitudeStream:
     def __init__(self, topic):
-        self.attitude_sub = rospy.Subscriber(topic, Quaternion, self.callback)
-        self.initial_euler_angles = # TODO
+        self.attitude_sub = rospy.Subscriber(topic, QuaternionStamped, self.callback)
+        self.euler_angles = 0
+        self.init_euler_angles = 0
+        self.init = False
 
     def callback(self, data):
-        euler_angles = self.Q2Euler(data.x, data.y, data.z, data.w)
+        euler_angles = self.Q2Euler(data)
         # TODO: Description.
-        local_euler_angles = euler_angles - self.initial_euler_angles
-        print(local_euler_angles)
-        return local_euler_angles
+        if not self.init:
+            self.init_euler_angles = euler_angles
+            self.init = True
+        self.euler_angles = euler_angles - self.init_euler_angles
+        print(self.euler_angles * 180 / pi)
+        return self.euler_angles
 
     # Convert a quaternion into euler angles.
     # Pitch is the rotation around the x-axis in radians.
@@ -27,17 +36,21 @@ class AttitudeStream:
     #  |  /
     #  | /
     #  |/
-    # ―|――――――――> x (pitch)
+    # -|--------> x (pitch)
     #
     # TODO: determine rl correspondances
-    def Q2Euler(self, x, y, z, w):
+    def Q2Euler(self, quaternion_stamped):
+        x = quaternion_stamped.quaternion.x
+        y = quaternion_stamped.quaternion.y
+        z = quaternion_stamped.quaternion.z
+        w = quaternion_stamped.quaternion.w
         """
         - x, y, z, w: the values of the quaternion.
 
         """
         t0 = 2. * (w*x + y*z)
         t1 = 1. - 2. * (x**2 + y**2)
-        roll_z = arctan2(t0, t1)
+        roll_z = -arctan2(t0, t1)
 
         t2 = 2. * (w*y - z*x)
         t2 = 1. if t2 > 1. else t2
@@ -50,3 +63,17 @@ class AttitudeStream:
 
         euler_angles = array([pitch_x, yaw_y, roll_z])
         return euler_angles
+
+
+def main():
+    AttitudeStream("/dji_sdk/attitude")
+    rospy.init_node("attitude_stream", anonymous=True)
+    try:
+        #print(self.euler_angles)
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down...") 
+
+
+if __name__ == '__main__':
+    main()
