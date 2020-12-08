ce2388c4ddc6aac519567baacb5042e5872970c2
Apollo <apollo@apollo.vision>
2020-12-07 21:26:43 -0800

    simulator fixes

--- util/dji_control.py
+++ util/dji_control.py
@@ -8,7 +8,7 @@ import math
 
 
 def main():
-    pub = rospy.Publisher('flight_control_setpoint_ENUposition_yaw', Joy, queue_size=10)
+    pub = rospy.Publisher('/dji_sdk/flight_control_setpoint_ENUposition_yaw', Joy, queue_size=10)
     rospy.init_node('control_planner')
 
     get_auth = rospy.ServiceProxy("/dji_sdk/sdk_control_authority", SDKControlAuthority)
@@ -33,49 +33,49 @@ def main():
         #j/l =rotate left/right
         #t/g = takeoff/land
         if (input == "1"):
-            msg.axes = [10, 0, 10, 0]
+            msg.axes = [1, 0, 1, 0]
             print("w: move forward 10 in x, at height 10. only publish once")
 
         elif (input == "2"):
-            msg.axes = [2, 0, 10, 0]
+            msg.axes = [2, 0, 1, 0]
             print("move forward by 2 in x, at height 10. publish 30 times")
             for i in range(30):
                 pub.publish(msg)
                 r.sleep()
         elif (input == "3"):
-            msg.axes = [10, 0, 10, 0]
+            msg.axes = [10, 0, 1, 0]
             print("move forward in x, at height 10. publish 30 times")
             for i in range(30):
                 pub.publish(msg)
                 r.sleep()
 
         elif (input == "4"):
-            msg.axes = [10, -10, 10, math.pi]
+            msg.axes = [10, -10, 1, math.pi]
             print("move forward in x, backward in y, yaw of pi, at height 10. publish 30 times")
             for i in range(30):
                 pub.publish(msg)
                 r.sleep()
 
         elif (input == "w"):
-            msg.axes = [2, 0, 10, 0]
+            msg.axes = [2, 0, 1, 0]
             print("w: move forward in x")
         elif (input == "s"):
-            msg.axes = [-2, 0, 10, 0]
+            msg.axes = [-2, 0, 1, 0]
             print("s: move backward in x")
         elif (input == "a"):
-            msg.axes = [0, -2, 10, 0]
+            msg.axes = [0, -2, 1, 0]
             print("a: move back in y")
         elif (input == "d"):
-            msg.axes = [0, 2, 10, 0]
+            msg.axes = [0, 2, 1, 0]
             print("d: move forward in y")
         elif (input == "j"):
-            msg.axes = [0, 0,10, -5]
+            msg.axes = [0, 0,1, -5]
             print("j: rotate with negative yaw")
         elif (input == "l"):
-            msg.axes = [0, 0, 10, 5]
+            msg.axes = [0, 0, 1, 5]
             print("l: rotate with positive yaw")
         elif (input == "i"):
-            msg.axes = [0, 0, 20, 0]
+            msg.axes = [0, 0, 1, 0]
             print("i: increase height. only works the first time")
         elif (input == "k"):
             msg.axes = [0, 0, 5, 0]
